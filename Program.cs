872ff72add2c3bb9a451337964293b54e4d99ce0
Marko-Pajic <pajic.marko84@gmail.com>
2024-04-05 12:57:22 +0200

    downloaded needed nuget packages

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Reading_Buddy
+﻿using HtmlAgilityPack;
+using System.Text;
+using VersOne.Epub;
+
+namespace Reading_Buddy
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = @"C:\Users\pajic\Downloads\pg2701-images-3.epub";
+            EpubBook book = EpubReader.ReadBook(filePath);
+            foreach (EpubLocalTextContentFile textContentFile in book.ReadingOrder)
+            {
+                PrintTextContentFile(textContentFile);
+            }
+        }
+
+        private static void PrintTextContentFile(EpubLocalTextContentFile textContentFile)
+        {
+            HtmlDocument htmlDocument = new();
+            htmlDocument.LoadHtml(textContentFile.Content);
+            StringBuilder sb = new();
+            foreach (HtmlNode node in htmlDocument.DocumentNode.SelectNodes("//text()"))
+            {
+                sb.AppendLine(node.InnerText.Trim());
+            }
+            string contentText = sb.ToString();
+            Console.WriteLine(contentText);
+            Console.WriteLine();
         }
     }
 }
