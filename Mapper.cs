ec5177f15867e3de35e0f4aea8817bf743914c67
Marko-Pajic <pajic.marko84@gmail.com>
2024-06-12 11:55:15 +0200

    comment out page building method and focus on chapter

--- Mapper.cs
+++ Mapper.cs
@@ -19,57 +19,57 @@ namespace Reading_Buddy
             b.Title = selectedBook.Title;
             b.Author = selectedBook.Author;
 
-            List<Page> pages = new List<Page>();
-            List<string> pageText = new List<string>();
-            int pageNumber = 1;
+            //List<Page> pages = new List<Page>();
+            //List<string> pageText = new List<string>();
+            //int pageNumber = 1;
 
-            HtmlDocument doc = new HtmlDocument();
+            //HtmlDocument doc = new HtmlDocument();
 
-            foreach (EpubLocalTextContentFile textContentFile in selectedBook.ReadingOrder)
-            {
-                doc.LoadHtml(textContentFile.Content);
-                string contentWithoutTags = doc.DocumentNode.InnerText;
-
-                string[] sentences = contentWithoutTags.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
-                foreach (string sentence in sentences)
-                {
-                    if (pageText.Count == 30)
-                    {
-                        pages.Add(new Page { Number = pageNumber.ToString(), Text = string.Join(" ", pageText) });
-                        pageText = new List<string>();
-                        pageNumber++;
-                    }
-                    pageText.Add(sentence.Trim());
-                }
-            }
-
-            if (pageText.Count > 0)
-            {
-                pages.Add(new Page { Number = pageNumber.ToString(), Text = string.Join(" ", pageText) });
-            }
-
-            b.Pages = pages;
-
-            return b;
-
-
-
-            //foreach (var n in selectedBook.Navigation)
+            //foreach (EpubLocalTextContentFile textContentFile in selectedBook.ReadingOrder)
             //{
-            //    Chapter chapter = new();
-            //    chapter.Title = n.Title;
-            //    EpubLocalTextContentFile contentFile = (EpubLocalTextContentFile)selectedBook.Content.AllFiles.Local.First(i => i.ContentLocation == n.HtmlContentFile.ContentLocation);
+            //    doc.LoadHtml(textContentFile.Content);
+            //    string contentWithoutTags = doc.DocumentNode.InnerText;
 
-            //    chapter.HTML = contentFile.Content;
-            //    //TODO Parse HTML
-
-            //    b.Chapters.Add(chapter);
-
-
+            //    string[] sentences = contentWithoutTags.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            //    foreach (string sentence in sentences)
+            //    {
+            //        if (pageText.Count == 30)
+            //        {
+            //            pages.Add(new Page { Number = pageNumber.ToString(), Text = string.Join(" ", pageText) });
+            //            pageText = new List<string>();
+            //            pageNumber++;
+            //        }
+            //        pageText.Add(sentence.Trim());
+            //    }
             //}
 
+            //if (pageText.Count > 0)
+            //{
+            //    pages.Add(new Page { Number = pageNumber.ToString(), Text = string.Join(" ", pageText) });
+            //}
+
+            //b.Pages = pages;
 
             //return b;
+
+
+
+            foreach (var n in selectedBook.Navigation)
+            {
+                Chapter chapter = new();
+                chapter.Title = n.Title;
+                EpubLocalTextContentFile contentFile = (EpubLocalTextContentFile)selectedBook.Content.AllFiles.Local.First(i => i.ContentLocation == n.HtmlContentFile.ContentLocation);
+
+                chapter.HTML = contentFile.Content;
+                //TODO Parse HTML
+
+                b.Chapters.Add(chapter);
+
+
+            }
+
+
+            return b;
         }
     }
 }
